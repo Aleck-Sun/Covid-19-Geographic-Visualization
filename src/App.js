08845837bc77fa01e1bee4f9466caea1f9bbb6f7
Aleck Bowen Sun <aleck.bowen.sun@gmail.com>
2021-02-14 20:06:53 -0500

    map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
+import ReactMapGL, {Marker} from 'react-map-gl';
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude:	34.695645,
+    longitude:  -30.514382,
+    width: "100vw",
+    height: "100vh",
+    zoom: 2
+  });
+
   return (
     <div className="App">
-      YOYOYOYYO
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/abs-development/ckl3yn84m2vqp17nqkpx0glnh"
+        onViewportChange={viewport => {setViewport(viewport);}}
+      >
+      </ReactMapGL>
     </div>
   );
 }
