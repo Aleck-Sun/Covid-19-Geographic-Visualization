888c5cb3db326aad7a0eb91924c9a6526566ba32
Aleck Bowen Sun <aleck.bowen.sun@gmail.com>
2021-02-16 22:35:40 -0500

    Added fetching data functionality to get country covid data and coordinates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactMapGL, {Marker} from 'react-map-gl';
+import { combine } from './api/index.js';
 
 function App() {
+  // Gets the data for all the countries
+  const [countriesData, setCountriesData] = useState([]);
+
+  useEffect(() => {
+    const data = combine();
+    Promise.resolve(data)
+      .then((datas) => {
+        setCountriesData(datas);
+      })
+  }, []);
+
+  // Initial viewport hook
   const [viewport, setViewport] = useState({
     latitude:	34.695645,
     longitude:  -30.514382,
@@ -19,6 +32,18 @@ function App() {
         mapStyle="mapbox://styles/abs-development/ckl3yn84m2vqp17nqkpx0glnh"
         onViewportChange={viewport => {setViewport(viewport);}}
       >
+        {countriesData.length > 0 ? countriesData.map(country => {
+          console.log("creating marker");
+          <Marker
+            latitude = {country.Latitude}
+            longitude = {country.Longitude}>
+              <div id="countryCases">
+                <button id="marker-btn">
+                  <img src="https://cdn2.iconfinder.com/data/icons/picons-basic-2/57/basic2-059_pin_location-512.png" />
+                </button>
+              </div>
+          </Marker>
+        }) : 'Loading...'}
       </ReactMapGL>
     </div>
   );
---
+++ src/api/index.js
@@ -0,0 +1,91 @@
+import data from '../data/countries.csv';
+import summary from '../data/summary.json';
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+// Gets data for every country
+const getCountries = async () => {
+    const URL = "https://api.covid19api.com/summary";
+    return fetch(URL, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            if (data) {
+                const countries = data.Countries.map(country => {
+                    var deathRecoveryRate = country.TotalRecovered/country.TotalDeaths;
+                    return country = {
+                        "CountryCode": country.CountryCode,
+                        "Cases": country.TotalConfirmed,
+                        "Deaths": country.TotalDeaths,
+                        "Recovered": country.TotalRecovered,
+                        "DeathRecoveryRate": deathRecoveryRate
+                    };
+                });
+                // Sorts the data by country code
+                countries.sort((a, b) => (a.CountryCode > b.CountryCode) - (a.CountryCode < b.CountryCode));
+                return countries;
+            };
+        })
+        .catch((error) => console.log(error));
+};
+
+// Gets the lat and longitude of all countries
+const getData = async () => {
+    return fetch(data)
+        .then(response => response.text())
+        .then(data => {
+            if (data) {
+                var countryCoordinates = [];
+                const rows = data.split('\n');
+                for (var i = 0; i < rows.length; i++) {
+                    var currRow = rows[i].split(',');
+                    countryCoordinates.push({
+                        "CountryName": currRow[3],
+                        "Latitude": currRow[1],
+                        "Longitude": currRow[2]
+                    });
+                };
+                return countryCoordinates;
+            };
+        })
+        .catch((error) => console.log(error));
+};
+
+// Gets the country cases from json file
+const getCountry = async () => {
+    const countries = summary.Countries.map(country => {
+        var deathRecoveryRate = country.TotalRecovered/country.TotalDeaths;
+        return country = {
+            "CountryCode": country.CountryCode,
+            "Cases": country.TotalConfirmed,
+            "Deaths": country.TotalDeaths,
+            "Recovered": country.TotalRecovered,
+            "DeathRecoveryRate": deathRecoveryRate
+        };
+    });
+    // Sorts the data by country code
+    countries.sort((a, b) => (a.CountryCode > b.CountryCode) - (a.CountryCode < b.CountryCode));
+    return countries;
+};
+
+export const combine = async () => {
+    // If you have API KEY you can get updated data using getCountries
+    // const covidData = await getCountries();
+    const covidData = await getCountry();
+    const coorData = await getData();
+    var countryData = [];
+    for (var i = 0; i < coorData.length; i++) {
+        countryData.push({
+            "CountryName": coorData[i].CountryName,
+            "Latitude": coorData[i].Latitude,
+            "Longitude": coorData[i].Longitude,
+            "Cases": covidData[i].Cases,
+            "Deaths": covidData[i].Deaths,
+            "Recovered": covidData[i].Recovered,
+            "DeathRecoveryRate": covidData[i].DeathRecoveryRate
+        });
+    };
+    return countryData;
+};
